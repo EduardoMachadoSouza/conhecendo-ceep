f2b5b7137e2407cb1b18d3ea5e3beb17cc633e13
Eduardo Machado de Souza <e.souza19@escola.pr.gov.br>
2023-08-27 14:54:15 -0300

    pub

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,5 +1,15 @@
-    <footer>
-        <h1>bruh²</h1>
+<footer>
+        <div class="conteiner-footer-bloco"></div>
+            <div class="conteiner-footer-itens">
+                <section class="social">
+                    <ul>
+                        <li><a href="https://www.instagram.com"><img src="img/ri_instagram-fill.svg" alt="logo Instragram"></a></li>
+                        <li><a href="https://www.facebook.com"><img src="img/ic_baseline-facebook.svg" alt="logo Facebook"></a></li>
+                    </ul>
+                </section>
+            </div>
+            <hr>
+            <h5>&copy; Todos os direitos reservados.</h5>
     </footer>
-</body>
+    </body>
 </html>
